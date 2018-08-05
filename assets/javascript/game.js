55ce3fe787082de4249b54be9d40d66ad4d4c31c
Rane M Kaseman <raykman@comcast.net>
2018-08-04 23:05:27 -0600

    2305

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
 
-    var targetScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    var targetScore = Math.floor(Math.random() * (30 - 19 + 1)) + 19;
     var playerTotal = 0;
     var wins = 0;
     var losses = 0;
@@ -24,19 +24,40 @@ $(document).ready(function() {
     $("#losses").text(losses);
     console.log("losses [ " + losses + " ]");
 
+
+    function reset(){
+        console.log("--reset--");
+        targetScore = Math.floor(Math.random() * (30 - 19 + 1)) + 19;
+        $("#targetScore").text(targetScore);
+        console.log("reset targetScore [ " + targetScore + " ]");
+
+        button1.setAttribute("value", Math.floor(Math.random() * (12 - 1 + 1)) + 1);
+        console.log("reset button1 = " + button1.value);
+
+        $("#playerTotal").text(playerTotal = 0);
+        console.log("reset playerTotal = [ " + playerTotal + " ]");
+        console.log("--------");
+    } 
+
+
     $("#button1").on("click", function() {
         $("#playerTotal").text(playerTotal + x);
         console.log("playerTotal + x " + (playerTotal + x));
         playerTotal += x;
         console.log("playerTotal " + (playerTotal));
-
         $("#messages").text("...waiting");
 
         if (playerTotal === targetScore) {
             $("#messages").text("...win!");
+            wins++;
+            $("#wins").text(wins);
+            reset();
         }
             else if (playerTotal >= targetScore) {
                 $("#messages").text("...try again.");
+                losses++;
+                $("#losses").text(losses);
+                reset();
             }
     });
 
