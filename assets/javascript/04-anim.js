7ae4782ebf5ac20cd7add2d0002e13c7528b3a0d
Rane M Kaseman <raykman@comcast.net>
2018-11-12 19:47:34 -0700

    housekeeping 1947

--- assets/javascript/04-anim.js
+++ assets/javascript/04-anim.js
@@ -3,7 +3,7 @@ var sketchProc = function (processingInstance) {
     with (processingInstance) {
         size(512, 400);
         frameRate(30);
-        background(0, 68, 136);
+        background(0, 34, 102);
         strokeWeight(2);
 
         fishColor = color(random(255), random(255), random(255));
@@ -25,10 +25,11 @@ var sketchProc = function (processingInstance) {
             ellipse(centerX + bodyLength / 4, centerY, bodyHeight / 5, bodyHeight / 5);
         };
 
+        // on button click, randomizes fish appearance
         $("#button1").click(function () {
             console.log("this: " + this.id)
             noStroke();
-            fill(0, 68, 136);
+            fill(0, 34, 102);
             rect(0, 0, 256, 200);
             drawFish(162, 100, random(40, 160), random(64, 72), random(fishColor));
         });
@@ -36,7 +37,7 @@ var sketchProc = function (processingInstance) {
         $("#button2").click(function () {
             console.log("this: " + this.id)
             noStroke();
-            fill(0, 68, 136);
+            fill(0, 34, 102);
             rect(256, 0, 256, 200);
             drawFish(416, 100, random(40, 160), random(64, 72), random(fishColor));
         });
@@ -44,7 +45,7 @@ var sketchProc = function (processingInstance) {
         $("#button3").click(function () {
             console.log("this: " + this.id)
             noStroke();
-            fill(0, 68, 136);
+            fill(0, 34, 102);
             rect(0, 200, 256, 200);
             drawFish(162, 300, random(40, 160), random(64, 72), random(fishColor));
         });
@@ -52,7 +53,7 @@ var sketchProc = function (processingInstance) {
         $("#button4").click(function () {
             console.log("this: " + this.id)
             noStroke();
-            fill(0, 68, 136);
+            fill(0, 34, 102);
             rect(256, 200, 256, 200);
             drawFish(416, 300, random(40, 160), random(64, 72), random(fishColor));
         });
@@ -62,17 +63,6 @@ var sketchProc = function (processingInstance) {
         drawFish(416, 100, random(40, 160), random(64, 72), random(fishColor));
         drawFish(162, 300, random(40, 160), random(64, 72), random(fishColor));
         drawFish(416, 300, random(40, 160), random(64, 72), random(fishColor));
-
-        // draw = function () {
-        //     if (y > 0) {
-        //         fill(0, 68, 136);
-        //         rect(192, 0, 48, 56);
-        //         fill(255, 0, 0);
-        //         ellipse(216, y, 10, 10);
-        //         y = y - 4;
-        //         console.log("y: " + y);
-        //     };
-        // };
 
     };
 };
